6fdad557561de00318678f8c7a34b073601ab79a
pedro <pedro1.ruckauf1@gmail.com>
2025-04-30 11:44:56 -0300

    prueba js

---
+++ curso inicial html/modulo 1/modulo 1 2.js
@@ -0,0 +1,6 @@
+function cuanta () {
+const promediop = 120000 ;
+const pelosperdidos = prompt ("ingreso los pelos los pelos que pierde por dia", '');
+const tiempopp = promediop/pelosperdidos ;
+document.writeln ('se quedara pelado en ${tiempopp}')
+}
